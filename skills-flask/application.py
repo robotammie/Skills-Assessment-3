6dd96c85b7446250ddb1c22d69220817bb00dfac
U-Lenovo\Tammie <tammie.kahnhauser@gmail.com>
2016-04-23 23:52:51 -0700

    completed basic jinja template for response page

--- skills-flask/application.py
+++ skills-flask/application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,20 @@ def index_page():
     # and return that result of rendering this, like:
     #
     # return render_template("index.html")
+
+
+@app.route("/application", methods=["POST"])
+def application_page():
+    firstname = request.form.get("firstname")
+    lastname = request.form.get("lastname")
+    salary = int(request.form.get("salary"))
+    job_title = request.form.get("position")
+
+    return render_template("application-response.html",
+                           firstname=firstname,
+                           lastname=lastname,
+                           salary=salary,
+                           job_title=job_title)
 
 
 if __name__ == "__main__":
